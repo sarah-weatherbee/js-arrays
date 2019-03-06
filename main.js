da67689f1d6ae636df838e6967ef63663c363e4d
Sarah Weatherbee <sarahmweatherbee@gmail.com>
2019-03-05 19:30:47 -0600

    wrote print to dom fxn

--- main.js
+++ main.js
@@ -1,1 +1,44 @@
+//const printToDom = (divId, textToPrint) => {
+ //   const selectedDiv = document.getElementById(divId);
+ //   selectedDiv.innerHTML += textToPrint;
+
 console.log('hi');
+
+const dinosaurs = [
+    {
+        dinoType: 't-rex'
+
+    },
+    {
+        dinoType: 'stegosaurus'
+
+    },
+    {
+        dinoType: 'velociraptor'
+
+    }
+];
+
+//loop over the dinosaurs
+//build up a dom string
+//print the dom string to the dom (will be an =)
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToPrint;
+
+};
+
+const buildDinosaurs = () => {
+    let domString = '';
+    for(let i = 0; i < dinosaurs.length; i++){
+        domString += `<h3>${dinosaurs[i].dinoType}</h3>`;
+        
+    }
+    printToDom('dino-barn', domString)
+};
+
+const init = () => {
+    buildDinosaurs();
+
+};
+init();
